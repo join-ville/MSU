e013d73b22ab32aaa81839dadf142ef45463cbbf
Go-Go-Zeppeli <1084320116@qq.com>
2020-06-19 09:29:16 +0800

    Merge branch 'master' of https://github.com/join-ville/MSU
    # Conflicts:
    #	src/router/router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,4 @@
 import App from '../app'
-import changename from "../frames/me/personaldetails/change/changename";
-import changesex from "../frames/me/personaldetails/change/changesex";
-import changeword from "../frames/me/personaldetails/change/changeword";
 
 const dialogue = r => require.ensure([], () => r(require('../frames/dialogue/dialogue')), 'dialogue')
 const singlechat = r => require.ensure([], () => r(require('../frames/conversation/singlechat')), 'singlechat')
@@ -133,20 +130,6 @@ export default[{
 				},
 				{
 					path:'/me/personaldetails',component : personaldetails,		//个人信息
-           children: [
-             {
-               path:'/me/personaldetails/changename',
-               component:changename,
-             },   //设置昵称
-             {
-               path:'/me/personaldetails/changesex',
-               component:changesex,
-             },   //设置性别
-             {
-               path:'/me/personaldetails/changeword',
-               component:changeword,
-             },   //设置个性签名
-           ]
 				},
 				{
 					path:'/me/cardbag',component : cardbag,		//卡包
